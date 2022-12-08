fa30c149364886505b4da6164eabde3662430f89
Rickard Nyman <4slovb@gmail.com>
2022-12-08 09:06:41 +0100

    Some types for good measure

--- Day08/First.hs
+++ Day08/First.hs
@@ -5,39 +5,49 @@ import System.IO
 
 import Data.Char ( digitToInt )
 
+type Position = (Int, Int)
+
+type StepGenerator = Position -> [Position]
+
+type PositionMap = Position -> Int
+
+newtype Limit = Limit Int
+unLimit :: Limit -> Int
+unLimit (Limit limit) = limit
+
 readInput :: [String] -> [[Int]]
 readInput = map readLine
 
 readLine :: String -> [Int]
 readLine = map digitToInt
 
-left :: Int -> Int -> Int -> [(Int, Int)]
-left limit x y = [(x - i, y) | i <- [1..(x - limit)]]
-
-right :: Int -> Int -> Int -> [(Int, Int)]
-right limit x y = [(x + i, y) | i <- [1..(limit - x)]]
-
-up :: Int -> Int -> Int -> [(Int, Int)]
-up limit x y = [(x, y - i) | i <- [1..(y - limit)]]
-
-down :: Int -> Int -> Int -> [(Int, Int)]
-down limit x y = [(x, y + i) | i <- [1..(limit - y)]]
-
-at :: [[Int]] -> (Int, Int) -> Int
+at :: [[Int]] -> PositionMap
 at trees (x, y) = trees !! y !! x
 
-limitedDirections :: Int -> [Int -> Int -> [(Int, Int)]]
-limitedDirections side = [limitLeft, limitRight, limitUp, limitDown]
-    where
-        limitLeft = left 0
-        limitRight = right (side - 1)
-        limitUp = up 0
-        limitDown = down (side - 1)
+left :: Limit -> StepGenerator
+left (Limit limit) (x, y) = [(x - i, y) | i <- [1..(x - limit)]]
 
-testDirections :: [Int -> Int -> [(Int, Int)]] -> ((Int, Int) -> Int) -> Int -> Int -> Bool
-testDirections dirs atTrees x y = do
-    let h = atTrees (x, y)
-    or [ all ((<h) . atTrees) (dir x y) | dir <- dirs ]
+right :: Limit -> StepGenerator
+right (Limit limit) (x, y) = [(x + i, y) | i <- [1..(limit - x)]]
+
+up :: Limit -> StepGenerator
+up (Limit limit) (x, y) = [(x, y - i) | i <- [1..(y - limit)]]
+
+down :: Limit -> StepGenerator
+down (Limit limit) (x, y) = [(x, y + i) | i <- [1..(limit - y)]]
+
+limitedDirections :: Limit -> [StepGenerator]
+limitedDirections limit = [limitLeft, limitRight, limitUp, limitDown]
+    where
+        limitLeft = left (Limit 0)
+        limitRight = right limit
+        limitUp = up (Limit 0)
+        limitDown = down limit
+
+testDirections :: [StepGenerator] -> PositionMap -> Position -> Bool
+testDirections dirs atTrees p = do
+    let h = atTrees p
+    or [ all ((<h) . atTrees) (dir p) | dir <- dirs ]
 
 main :: IO ()
 main = do
@@ -50,8 +60,8 @@ main = do
     let trees = readInput xs
     -- print trees
     let atTrees = at trees
-    let side = length trees
-    let dirs = limitedDirections side
-    let tested = [ testDirections dirs atTrees x y | x <- [0..(side - 1)], y <- [0..(side - 1)] ]
+    let lim = length trees - 1
+    let dirs = limitedDirections (Limit lim)
+    let tested = [ testDirections dirs atTrees (x, y) | x <- [0..lim], y <- [0..lim] ]
     print $ length (filter id tested)
     hClose handler
